75cfaf029e87bdd1d971b1f8ded30c7b08e8e7a6
Antoine Kociuba <antoine.kociuba@artifakt.io>
2021-07-07 12:20:00 +0200

    Test

--- artifakt/deploy.sh
+++ artifakt/deploy.sh
@@ -10,17 +10,19 @@ echo $tailPath;
 echo $headPath;
 echo $headPathEscaped;
 
+env
+
 # Custom NGINX rewrites file
 if [[ -z $(grep "include $headPath/current/artifakt/nginx_rewrites.conf" "/etc/nginx/sites-available/magento") ]]; then
     if [[ -f "$currentPath/artifakt/nginx_rewrites.conf" ]]; then
-        sed -i "/server {/ i\ include $headPathEscaped\/current\/artifakt\/nginx_rewrites.conf;" /etc/nginx/sites-available/magento;
+        sudo sed -i "/server {/ i\ include $headPathEscaped\/current\/artifakt\/nginx_rewrites.conf;" /etc/nginx/sites-available/magento;
         echo "Custom rewrites file has been included into NGINX configuration"
     else
         echo "Custom rewrites file has not been included into NGINX configuration as it does not exist"
     fi
 else
     if [[ ! -f "$currentPath/artifakt/nginx_rewrites.conf" ]]; then
-        sed -i "/include $headPathEscaped\/current\/artifakt\/nginx_rewrites.conf;/d" /etc/nginx/sites-available/magento
+        sudo sed -i "/include $headPathEscaped\/current\/artifakt\/nginx_rewrites.conf;/d" /etc/nginx/sites-available/magento
         echo "Custom rewrites file has been removed from NGINX configuration as the file does not exist anymore"
     else
         echo "Custom rewrites file has already been included into NGINX configuration";
